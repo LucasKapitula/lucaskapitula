becea5f5a07eb7d03f90c4daa84258b63c3c3a34
LucasKapitula <124095370+LucasKapitula@users.noreply.github.com>
2023-01-31 17:59:25 -0300

    Add files via upload

---
+++ Proyecto Final Integrador/datos.js
@@ -0,0 +1,12 @@
+
+var curriculum_lista  = [
+    "<li><div class='container'><img class='imagen' src= personas/87.jpg alt='foto currículum'></img><br><h2>Soham Bell</h2><br><div class='datos'><img class='icono' src= img/mail.svg alt='icono de mail'>Mail: soham.bell@example.com<br><img class='icono' src= img/calendario.svg alt='icono calendario'>Nacimiento: 03/07/1970<br><img class='icono' src= img/Direccion.svg alt='icono direccion'>Dirección: 7279 Camden Av.<br><img class='icono' src= img/telefono.svg alt='icono telefono'>Teléfono: (819) 421-0136<br><img class='icono' src= img/contrasena.svg alt='icono contraseña'>Contraseña: Dark</div></div></li>",/*bloque de datos de soham bell*/
+    
+    "<li><div class='container'><img class='imagen' src= personas/65.jpg alt='foto currículum'></img><br><h2>Glenda Vázquez</h2><br><div class='datos'><img class='icono' src= img/mail.svg alt='icono de mail'>Mail: glenda.vazquez@example.com<br><img class='icono' src= img/calendario.svg alt='icono calendario'>Nacimiento: 20/06/1964<br><img class='icono' src= img/Direccion.svg alt='icono direccion'>Dirección: 3053 Green Rd<br><img class='icono' src= img/telefono.svg alt='icono telefono'>Teléfono: (977) 968-6614<br><img class='icono' src= img/contrasena.svg alt='icono contraseña'>Contraseña: Windows</div></div></li>",/*bloque de datos de glenda vazquez*/
+    
+    "<li><div class='container'><img class='imagen' src= personas/01.jpg alt='foto currículum'></img><br><h2>Lucas Kapitula</h2><br><div class='datos'><img class='icono' src= img/mail.svg alt='icono de mail'>Mail: lucaskapitulaz@gmail.com<br><img class='icono' src= img/calendario.svg alt='icono calendario'>Nacimiento: 20/06/1994<br><img class='icono' src= img/Direccion.svg alt='icono direccion'>Dirección: 5965 Sarachaga<br><img class='icono' src= img/telefono.svg alt='icono telefono'>Teléfono: (+54) 11-6680-6167<br><img class='icono' src= img/contrasena.svg alt='icono contraseña'>Contraseña: Curso</div></div></li>",/*bloque de datos de lucas kapitula*/
+]
+
+    
+    for(i=0;i<=curriculum_lista.length - 1;i++){
+        document.write (curriculum_lista[i]);}
